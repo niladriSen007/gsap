b2cbdaf4e8db090fce597443e1fa0df1a00d9b2b
Niladri <niladrisengit12@gmail.com>
2023-07-29 12:33:10 +0530

    project 1

---
+++ project-1/app.js
@@ -0,0 +1,54 @@
+// gsap.to("#box1",{
+//     x:800,
+//     y:700,
+//     duration:2.5,
+//     delay:2,
+//     backgroundColor:"indigo",
+//     rotate:180,
+//     borderRadius:"100%",
+// })
+// gsap.from("#box1",{
+//     x:800,
+//     y:700,
+//     duration:2.5,
+//     delay:2,
+//     backgroundColor:"red",
+//     rotate:180,
+//     borderRadius:"0%",
+// })
+
+
+// gsap.to("#box1",{
+//     x:1680,
+//     y:0,
+//     duration:2.5,
+//     delay:4.5,
+//     backgroundColor:"red",
+//     rotate:180,
+//     borderRadius:"0%",
+// })
+
+
+gsap.to("#box1",{
+    x:0,
+    y:700,
+    duration:1,
+    delay:2,
+    backgroundColor:"red",
+    repeat:-1,
+    yoyo:true,
+    // rotate:180,
+    borderRadius:"100%",
+})
+
+
+
+// gsap.to("h1",{
+//     x:1200,
+//     duration:2.5,
+//     delay:2,
+//     color:"red",
+//     // stagger:1,
+//     repeat:-1,
+//     yoyo:true
+// })
